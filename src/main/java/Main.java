03ea56bc26b337b2882d0e7fce700e13c19305ef
Zara116 <zarema.mindubaeva@yandex.ru>
2022-08-10 20:36:21 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(1000, true);
+
+        amount = 1_000_000;
+        registered = true;
+        expected = 500;
+        actual = service.calculate(1_000_000, true);
+
+        amount = 100;
+        registered = false;
+        expected = 1;
+        actual = service.calculate(100, false);
+
+        amount = 400_000;
+        registered = false;
+        expected = 1;
+        actual = service.calculate(400_000, false);
+
+        //System.out.println(expected + "==?=="+actual);
+
+    }
+}
